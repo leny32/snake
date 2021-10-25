7781355ac8c05d852ea79113a8b0742c1a9ef7d7
helhost <92789913+helhost@users.noreply.github.com>
2021-10-25 12:51:16 +0200

    port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/public')); //__dir and not _dir
-var port = 5500; // you can use any port
+var port = 4598; // you can use any port
 app.listen(port);
 console.log('server on' + port);
