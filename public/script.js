2b3031a7fc01582295a11002dac48d5a3e898693
leny32 <40176071+yezzl@users.noreply.github.com>
2021-11-30 00:21:00 +0100

    OPPPDATOS

--- public/script.js
+++ public/script.js
@@ -1,148 +1,142 @@
 let posx = 0;
 let posy = 0;
-let direction = null;
+let pastDirection = null;
+let currentDirection = 'right';
 let interval = 200;
 let t = setInterval(move, interval);
 let appleCords = { x: 0, y: 0 };
 let score = -1;
-let localStorage = window.localStorage;
-let highscore = localStorage.getItem('Highscore') || 0;
 let oldMoves = [{ x: 0, y: 0 }];
 let bodylength = 0;
-let allSqures = [];
+let allSquares = [];
 let moved = false;
-let upDir = 'up';
-let downDir = 'down';
-let leftDir = 'left';
-let rightDir = 'right';
-let diff = 1 ;
+let startTime = 6000;
+let audio_timer = new Audio('media/audio_timer.mp3');
+let audio_t3 = new Audio('media/audio_scifi.mp3');
+let audio_t4 = new Audio('media/audio_beep.mp3');
+audio_timer.play();
+audio_timer.volume = 0.2;
+audio_t3.volume = 0.4;
+audio_t4.volume = 0.5;
+audio_t3.loop = true;
+audio_t4.loop = true;
 
 /*------------------------------------------------------------*/
 
-window.onload = function () {
-	generateApple();
-	changeDifficulty(1);
-}
-
 genAllSquares();
-
-function changeSpeed() {
-	interval = interval == 200 ? 150 : interval == 150 ? 100 : interval == 100 ? 400 : interval == 400 ? 300 : 200
-	clearInterval(t)
-	t = setInterval(move, interval)
-}
-function changeDifficulty(difficulty){
-	diff = difficulty;
-	switch (difficulty){
-		case 1:
-			downDir = 'down';
-			upDir = 'up';
-			leftDir = 'left';
-			rightDir = 'right';
-			break;
-		case 2:
-			downDir = 'up';
-			upDir = 'down';
-			leftDir = 'right';
-			rightDir = 'left';
-			break;
-		case 3:
-			downDir = 'left';
-			upDir = 'right';
-			leftDir = 'down';
-			rightDir = 'up';
-			break;
-		case 4:
-			downDir = 'up';
-			upDir = 'right';
-			leftDir = 'down';
-			rightDir = 'left';
-			break;
+generateApple();
+window.addEventListener('message', (event) => {
+	let data = event.data
+	console.log("test")
+	if (data.action == 'startGame') {
+		startGame();
 	}
+});
+
+
+function startGame() {
+	posx = 0;
+	posy = 0;
+	score = -1;
+
+	let element = document.getElementById("bodyFolder");
+	while (element.firstChild) {
+		element.removeChild(element.firstChild);
+	}
+
+	oldMoves = [{ x: 0, y: 0 }];
+	allSquares = [];
+	document.getElementById('box').style.left = 1 + 'px';
+	document.getElementById('box').style.top = 1 + 'px';
+	return;
 }
 
+let x = setInterval(() => {
+	if (score == 10) {
+		audio_t3.play();
+	} else if (score == 15) {
+		audio_t3.pause();
+		audio_t4.play();
+	} else if (score == 20) {
+		audio_t3.play();
+	}
+	startTime--;
+	let sec = Math.floor(startTime / 100);
+	let ms = startTime % 100;
+	if (startTime < 1) {
+		clearInterval(x);
+		failedGame();
+	}
+	let tr = document.getElementById('score');
+	tr.innerHTML = `${sec < 10 ? '0' + sec : sec}<span id="ms">${ms}</span>`;
+}, 10);
 
 document.addEventListener('keydown', keyTask);
 function keyTask(e) {
-	if (moved) return;
 	switch (e.code) {
 		case 'ArrowUp':
-			direction = direction == downDir ? downDir : upDir;
-			moved = true;
+		case 'KeyW':
+			currentDirection = pastDirection == 'down' ? 'down' : 'up';
 			break;
 		case 'ArrowDown':
-			direction = direction == upDir ? upDir : downDir;
-			moved = true;
+		case 'KeyS':
+			currentDirection = pastDirection == 'up' ? 'up' : 'down';
 			break;
 		case 'ArrowLeft':
-			direction = direction == rightDir ? rightDir : leftDir;
-			moved = true;
+		case 'KeyA':
+			currentDirection = pastDirection == 'right' ? 'right' : 'left';
 			break;
 		case 'ArrowRight':
-			direction = direction == leftDir ? leftDir : rightDir;
-			moved = true;
-			break;
-		case 'KeyW':
-			direction = direction == downDir ? downDir : upDir;
-			moved = true;
-			break;
-		case 'KeyS':
-			direction = direction == upDir ? upDir : downDir;
-			moved = true;
-			break;
-		case 'KeyA':
-			direction = direction == rightDir ? rightDir : leftDir;
-			moved = true;
-			break;
 		case 'KeyD':
-			direction = direction == leftDir ? leftDir : rightDir;
-			moved = true;
+			currentDirection = pastDirection == 'left' ? 'left' : 'right';
 			break;
-		case 'Space':
-			changeSpeed();
-			break;
-		case 'Digit1':
-			changeDifficulty(1);
-			console.log('1');
-			break;
-		case 'Digit2':
-			changeDifficulty(2);
-			console.log('2');
-			break;
-		case 'Digit3':
-			changeDifficulty(3);
-			console.log('3');
-			break;
-		case 'Digit4':
-			changeDifficulty(4);
-			console.log('4');
+		case 'Escape':
+			failedGame();
 			break;
 	}
 }
 
 function generateApple() {
-    let apple = document.getElementById("apple");
-    let working = removeOldMoves(allSqures, oldMoves);
+	let apple = document.getElementById("apple");
+	let working = removeOldMoves(allSquares, oldMoves);
 
 	let newApple = working[getRandomInt(working.length)];
-	app = {x: (newApple.x*50)-50, y: (newApple.y*50)-50};
+	app = { x: (newApple.x * 50) - 50, y: (newApple.y * 50) - 50 };
 
 	if (appleCords.x == app.x && appleCords.y == app.y) return generateApple();
 
-    apple.style.top = app.y + 'px';
-    apple.style.left =  app.x + 'px';
+	apple.style.top = app.y + 'px';
+	apple.style.left = app.x + 'px';
 
 	appleCords = { x: app.x, y: app.y };
 
-	score += diff;
-	if (score == 98*diff) {
-		highscore = 100*diff
-		clearInterval(t);
-		alert('You win!');
-	}
-
-    document.getElementById('score').innerHTML = `Poeng: ${score}</br> Highscore: ${highscore}`;
+	score++;
+	document.getElementById('box').innerHTML = score;
 };
+
+function verifyWin() {
+	clearInterval(t);
+	clearInterval(x);
+	audio_timer.pause();
+	//audio_t2.pause();
+	audio_t3.pause();
+	audio_t4.pause();
+	let element = document.getElementById("bodyFolder");
+	let i = 0;
+	while (element.firstChild) {
+		i++;
+		element.removeChild(element.firstChild);
+	};
+	if (i == 25) {
+		console.log();
+		fetch("http://nui2/win", {
+			method: "POST",
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ score: btoa(JSON.stringify(oldMoves)) })
+		});
+	}
+	return;
+}
 
 function findCollision(x, y) {
 	let coll = false;
@@ -154,9 +148,12 @@ function findCollision(x, y) {
 	return coll;
 }
 function move() {
+	if (score == 25) return verifyWin();
+	if (currentDirection == null) audio_timer.pause();
+	else audio_timer.play()
 	let oldx = posx;
 	let oldy = posy;
-	switch (direction) {
+	switch (currentDirection) {
 		case 'right':
 			posx += 50;
 			break;
@@ -170,87 +167,80 @@ function move() {
 			posy -= 50;
 			break;
 	}
-	
-	let xCord = (posx+50)/50;
-    let yCord = (posy+50)/50;
-	let currentPos = {x:xCord, y:yCord};
-	
+
+	let xCord = (posx + 50) / 50;
+	let yCord = (posy + 50) / 50;
+	let currentPos = { x: xCord, y: yCord };
+
 	let colliction = findCollision(xCord, yCord);
 	if ((posx < 0 || posy < 0 || posx > 450 || posy > 450) || colliction) return failedGame();
-	moved = false;
-	
+	pastDirection = currentDirection;
+
 	animateHead(oldx, posx, oldy, posy);
 
-	if (oldMoves.length > 1){
-		for (i=0; i <oldMoves.length-1; i++) {
-			let lastSquare = oldMoves[oldMoves.length-1-i];
-			let secondLastSquare = oldMoves[oldMoves.length-2-i];
+	if (oldMoves.length > 1) {
+		for (i = 0; i < oldMoves.length - 1; i++) {
+			let lastSquare = oldMoves[oldMoves.length - 1 - i];
+			let secondLastSquare = oldMoves[oldMoves.length - 2 - i];
 			let nodelength = document.getElementById('bodyFolder').childNodes.length;
-			animateTail(lastSquare.x, secondLastSquare.x, lastSquare.y, secondLastSquare.y, nodelength-1-i);
+			animTail(lastSquare.x, lastSquare.y, nodelength - 1 - i, secondLastSquare.x, secondLastSquare.y);
 		}
-	} 
+	}
 
-    if (appleCords.x == posx && appleCords.y == posy) {
+	if (appleCords.x == posx && appleCords.y == posy) {
 		generateApple();
 		oldMoves.unshift(currentPos);
 
-		if (oldMoves.length > 1){
+		if (oldMoves.length > 1) {
 			createTail();
-		}		
-		
-		if(highscore <= score) highscore = score + 1;
+		}
 
-
-   	} else if (posx != oldx || posy != oldy){
+	} else if (posx != oldx || posy != oldy) {
 		oldMoves.unshift(currentPos);
 		oldMoves.pop();
-		if (oldMoves.length > 1){
+		if (oldMoves.length > 1) {
 			moveTail();
 		}
 	}
+
 }
 
 function failedGame() {
-	posx = 0;
-	posy = 0;
-	score = -1*diff;
-	localStorage.setItem('Highscore', highscore);
-	
-	let element = document.getElementById("bodyFolder");
-	while (element.firstChild) {
-		element.removeChild(element.firstChild);
-	}
+	audio_timer.pause();
+	//audio_t2.pause();
+	audio_t3.pause();
+	audio_t4.pause();
+	clearInterval(x);
+	clearInterval(t);
 
-	generateApple()
-
-	oldMoves = [{ x: 0, y: 0 }];
-	allSqures = [];
-	genAllSquares();
-	document.getElementById('box').style.left = 1 + 'px';
-	document.getElementById('box').style.top = 1 + 'px';
-	return direction = null;
+	fetch("http://nui2/close", {
+		method: "POST",
+		headers: { 'Content-Type': 'application/json' },
+		body: JSON.stringify({ score: score })
+	})
+	return;
 }
 
 function animateHead(xfrom, xto, yfrom, yto) {
 	let x = xfrom;
 	let y = yfrom;
 
-	let id = setInterval(frame, interval / 50);
+	let id = setInterval(frame, interval / 25);
 	function frame() {
 		if (x == xto && y == yto) {
 			clearInterval(id);
 		} else {
 			if (x < xto) {
-				x++;
+				x += 2;
 			}
 			if (x > xto) {
-				x--;
+				x -= 2;
 			}
 			if (y < yto) {
-				y++;
+				y += 2;
 			}
 			if (y > yto) {
-				y--;
+				y -= 2;
 			}
 			document.getElementById('box').style.left = x + 'px';
 			document.getElementById('box').style.top = y + 'px';
@@ -258,36 +248,14 @@ function animateHead(xfrom, xto, yfrom, yto) {
 	}
 }
 
-function animateTail(xfrom, xto, yfrom, yto, bodypos) {
-	let x = (xfrom*50)-50;
-	let y = (yfrom*50)-50;
-	let x2 = (xto*50)-50;
-	let y2 = (yto*50)-50;
-
-	let id = setInterval(frame, interval / 50);
-	function frame() {
-		if (x == x2 && y == y2) {
-			clearInterval(id);
-		} else {
-			if (x < x2) {
-				x++;
-			}
-			if (x > x2) {
-				x--;
-			}
-			if (y < y2) {
-				y++;
-			}
-			if (y > y2) {
-				y--;
-			}
-			document.getElementById('bodyFolder').childNodes[bodypos].style.left = x + 'px';
-			document.getElementById('bodyFolder').childNodes[bodypos].style.top = y + 'px';
-		}
-	} 
+function animTail(xto, yto, bodypos) {
+	let x = (xto * 50) - 50;
+	let y = (yto * 50) - 50;
+	document.getElementById('bodyFolder').childNodes[bodypos].style.left = x + 'px';
+	document.getElementById('bodyFolder').childNodes[bodypos].style.top = y + 'px';
 }
 
-function createTail (){
+function createTail() {
 	let newBod = document.createElement('div');
 	newBod.setAttribute('id', 'bod');
 	document.getElementById('bodyFolder').appendChild(newBod);
@@ -295,38 +263,38 @@ function createTail (){
 	moveTail(a);
 }
 
-function moveTail (pos) {
-	if (Number.isInteger(pos)){
+function moveTail(pos) {
+	if (Number.isInteger(pos)) {
 		bodylength = pos;
 	}
-	for (let i = 1; i <= bodylength; i++){
-		document.getElementById('bodyFolder').childNodes[i-1].style.left = ((oldMoves[i].x)*50)-50 +'px';
-		document.getElementById('bodyFolder').childNodes[i-1].style.top = ((oldMoves[i].y)*50)-50 +'px';
+	for (let i = 1; i <= bodylength; i++) {
+		document.getElementById('bodyFolder').childNodes[i - 1].style.left = ((oldMoves[i].x) * 50) - 50 + 'px';
+		document.getElementById('bodyFolder').childNodes[i - 1].style.top = ((oldMoves[i].y) * 50) - 50 + 'px';
 	}
 }
 
-function getRandomInt (max) {
-	return Math.floor(Math.random()*max); 
+function getRandomInt(max) {
+	return Math.floor(Math.random() * max);
 }
 
 function genAllSquares() {
-	for (let i = 1; i <= 10; i++){
-		for (let j = 1; j <=10; j++){
-			allSqures.push({x: i, y: j});
+	for (let i = 1; i <= 10; i++) {
+		for (let j = 1; j <= 10; j++) {
+			allSquares.push({ x: i, y: j });
 		}
 	}
 }
 
 function removeOldMoves(arr1, arr2) {
 	let newArr = [];
-	for (let i = 0; i < arr1.length; i++){
+	for (let i = 0; i < arr1.length; i++) {
 		let match = false;
-		for (let j = 0; j < arr2.length; j++){
-			if (arr1[i].x == arr2[j].x && arr1[i].y == arr2[j].y){
+		for (let j = 0; j < arr2.length; j++) {
+			if (arr1[i].x == arr2[j].x && arr1[i].y == arr2[j].y) {
 				match = true;
 			}
 		}
-		if (!match){
+		if (!match) {
 			newArr.push(arr1[i]);
 		}
 	}
